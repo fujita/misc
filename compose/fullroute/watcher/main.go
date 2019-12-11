7cf47f938a109d0835c54bcc3eb2d9cefcb9f0a3
FUJITA Tomonori <fujita.tomonori@gmail.com>
2019-12-11 15:58:25 +0900

    compose/fullroute: add watcher
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

---
+++ compose/fullroute/watcher/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	api "github.com/osrg/gobgp/api"
+	"google.golang.org/grpc"
+)
+
+var (
+	peerOpt  = flag.Int("p", 8, "number of peers")
+	routeOpt = flag.Int64("r", 771684, "number of routes")
+)
+
+func main() {
+	flag.Parse()
+
+	nr := int64(*peerOpt) * *routeOpt
+	fmt.Println("wait for ", *peerOpt, " peers; each has ", *routeOpt, " routes.")
+
+	grpcOpts := []grpc.DialOption{grpc.WithBlock(), grpc.WithInsecure()}
+
+	conn, err := grpc.DialContext(context.Background(), "127.0.0.1:50051", grpcOpts...)
+	if err != nil {
+		fmt.Println("can't connect ", err)
+		os.Exit(0)
+	}
+
+	client := api.NewGobgpApiClient(conn)
+
+	init := false
+	var start time.Time
+	for {
+		stream, err := client.ListPeer(context.Background(), &api.ListPeerRequest{})
+		if err != nil {
+			fmt.Println("failed to ListPeer ", err)
+			os.Exit(0)
+		}
+
+		peers := 0
+		accepted := uint64(0)
+		for {
+			r, err := stream.Recv()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				fmt.Println("failed to parse the response ", err)
+				os.Exit(0)
+			}
+			if init == false {
+				init = true
+				start = time.Now()
+			}
+			peers++
+
+			for _, afisafi := range r.Peer.GetAfiSafis() {
+				accepted += afisafi.State.Accepted
+			}
+		}
+		if init {
+			fmt.Println(time.Now(), " ", peers, " peers ", accepted, " accepted")
+		}
+		if uint64(peers)*accepted == uint64(nr) {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	fmt.Println("finished: ", time.Since(start).Seconds(), " seconds")
+}
--- go.mod
+++ go.mod
@@ -8,4 +8,8 @@ require (
 	github.com/k-sone/critbitgo v1.2.0
 	github.com/osrg/gobgp v2.0.0+incompatible
 	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
+	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	google.golang.org/grpc v1.25.1
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
 )
